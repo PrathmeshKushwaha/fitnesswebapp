897ff29c25e93f97aa1298fcb78037e8e979f816
PrathmeshKushwaha <prathmshkush@gmail.com>
2024-03-01 13:08:48 +0530

    sixth push

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import DeclarativeBase
+from flask_login import LoginManager, login_user
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/database"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Disable modification tracking
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy()  # Bind SQLAlchemy instance with Flask app
 
-db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-class User(db.Model):
+class Users(db.Model):
     __tablename__ = 'user'
     uid = db.Column(db.Integer, primary_key=True)
     uname = db.Column(db.String)
+    password = db.Column(db.String)
     uemail = db.Column(db.String)
     uphone = db.Column(db.String)
     city = db.Column(db.String)
     gender = db.Column(db.String)
     height = db.Column(db.Float)
     weight = db.Column(db.Float)
-    profile = db.relationship('Profile', back_populates='user')
-    posts = db.relationship('Posts', back_populates='user')
-    udata = db.relationship('UserData', back_populates='user')
-    agenda = db.relationship('Agenda', back_populates='user')
-    explore = db.relationship('Explore', back_populates='user')
-    message = db.relationship('Message', back_populates='user')
+    profile = relationship('Profile', back_populates='user')
+    posts = relationship('Posts', back_populates='user')
+    udata = relationship('UserData', back_populates='user')
+    agenda = relationship('Agenda', back_populates='user')
+    explore = relationship('Explore', back_populates='user')
+    message = relationship('Message', back_populates='user')
 
 @app.route("/")
 def hello_world():
@@ -34,5 +38,39 @@ def hello_world():
 def dashboard():
     return render_template('Dashboard.html')
 
-with app.app_context():
-    db.create_all()
+@app.route('/agenda.html')
+def agenda():
+    return render_template('agenda.html')
+
+@app.route('/explore.html')
+def explore():
+    return render_template('explore.html')
+
+@login_manager.user_loader
+def loader_user(uid):
+    return Users.query.get(uid)
+
+@app.route('/signup.html', methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        user = Users(uname=request.form.get("username"),
+                     password=request.form.get("password"))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("signup.html")
+
+
+@app.route("/login.html", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            uname=request.form.get("username")).first()
+        if user.password == request.form.get("password"):
+            # Use the login_user method to log in the user
+            login_user(user)
+            return redirect(url_for("dashboard"))
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
